715129a40db0077466631945b15b83e9e0ad0ac6
tshinohara <takuma1201@gmail.com>
2024-02-23 14:03:14 +0000

    aerostreamの取得周り実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use aerostream::Client;
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = Client::default();
+    client.set_timeout(5);
+    let did = match client.get_handle("shino3.net") {
+        Ok(r) => r.clone(),
+        _ => String::from("UNKNOWN"),
+    };
+    let handle = client.get_repo(did);
+    println!("{:?}", handle);
 }
