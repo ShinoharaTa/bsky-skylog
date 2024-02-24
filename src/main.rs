973d1475412e757bb1509fb29d211503100bcb84
tshinohara <takuma1201@gmail.com>
2024-02-24 16:44:15 +0900

    投稿取得まで実装

--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,14 @@ async fn get_all_followers(
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    // 設定取得
     let config_path = "./config.json";
     let file_content = fs::read_to_string(config_path)
         .with_context(|| format!("設定ファイル {} の読み込みに失敗しました", config_path))?;
     let config: Config = serde_json::from_str(&file_content)
         .with_context(|| format!("設定ファイル {} の展開に失敗しました", config_path))?;
 
+    // クライアントの初期設定
     let mut client = Client::default();
     client
         .login(&config.handle, config.password)
@@ -46,9 +48,24 @@ async fn main() -> Result<(), anyhow::Error> {
         Ok(r) => r.clone(),
         _ => String::from("UNKNOWN"),
     };
+
+    // 全フォロワー取得
     let followers = get_all_followers(&client, &config.handle).await?;
     println!("{}", followers.len());
 
+    let getRecord = client.client.com_atproto_repo_listrecords(
+        &did,
+        "app.bsky.feed.post",
+        None,
+        None,
+        None,
+        None,
+        None,
+    );
+    //  (&follower.did, None)?;
+    println!("{:?}", getRecord);
+    for follower in followers {
+        // println!("{}");
+    }
     Ok(())
-    // let handle = client.get_repo(did);
 }
